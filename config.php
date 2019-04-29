cc87312b6ac8cd1bf3e12020c30398fae113eea7
Jabont <Jabont@hotmail.com>
2019-04-29 21:04:34 +0700

    ignorfile

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 	$CONFIG = array();
 	$site_name = 'Hermes';
 	$site_url = "https://jabont.com/dev/hermes/";
-	$CONFIG['db_name'] = 'hermes_db';
-	$CONFIG['db_user'] = 'hermes_db';
-	$CONFIG['db_password'] = 'hermesit16';
+	$CONFIG['db_name'] = '__';
+	$CONFIG['db_user'] = '__';
+	$CONFIG['db_password'] = '__';
 	$CONFIG['db_host'] = '161.246.38.35';
 	$CONFIG['db_charset'] = 'dbcharset_here';
 ?>
